be4fa561257e5267dd22340a65aa865bae34c14c
= <=>
2023-11-03 10:22:23 +0000

    Added the structure for user store

---
+++ src/store/user/index.js
@@ -0,0 +1,14 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state(){
+        return {
+            name: '',
+            age: 0,
+            house_number: ''
+        },
+        mutations: ,
+        actions: ,
+        getters: ,
+    }
+})
